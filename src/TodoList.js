8fd0fa1a76d612deba66e8fbfdf489c173db9996
赵灵雪 <zhaolingxue@zhaolinxuedeMBP.lan>
2019-05-09 16:41:58 +0800

    实现新增

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,21 +5,31 @@ class Todolist extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      inputValue: 'jkl',
-      value: []
+      inputValue: '',
+      list: []
     }
   }
   render() {
     return  (
       <div>
-        <div><input value={this.state.inputValue}  onChange={this.handleInputChange.bind(this)}/><button>提交</button></div>
+        <div><input value={this.state.inputValue}  onChange={this.handleInputChange.bind(this)}/>
+        <button onClick = {this.handleBtnChange.bind(this)}>提交</button></div>
         <ul>
-          <li>123</li>
-          <li>123</li>
-          <li>123</li>
+        {
+          this.state.list.map((item, index)=> {
+            return <li key={index}>{item}</li>
+          })
+        }
         </ul>
       </div>
     )
+  }
+  handleBtnChange(e) {
+    console.log('p')
+    this.setState({
+      list: [...this.state.list, this.state.inputValue]
+    })
+    
   }
   handleInputChange(e){
     this.setState({

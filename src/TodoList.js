21bd82781f65fc564f482f78a3b17d2cabb7f8d2
赵灵雪 <zhaolingxue@zhaolinxuedeMBP.lan>
2019-05-09 14:04:39 +0800

    state

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-function Todolist() {
+class Todolist extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: 'jkl',
+      value: []
+    }
+  }
+  render() {
     return  (
       <div>
-        <div><input/><button>提交</button></div>
+        <div><input value={this.state.inputValue}  onChange={this.handleInputChange.bind(this)}/><button>提交</button></div>
         <ul>
           <li>123</li>
           <li>123</li>
@@ -11,5 +20,12 @@ function Todolist() {
         </ul>
       </div>
     )
+  }
+  handleInputChange(e){
+    this.setState({
+      inputValue: e.target.value
+    })
+  } 
+   
 }
 export default Todolist;

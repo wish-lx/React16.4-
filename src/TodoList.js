15042deb08ea2882b368e526bbe290871484472e
赵灵雪 <zhaolingxue@zhaolinxuedeMBP.lan>
2019-05-09 13:35:14 +0800

    todoList

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Todolist() {
+    return  (
+      <div>
+        <div><input/><button>提交</button></div>
+        <ul>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+        </ul>
+      </div>
+    )
+}
+export default Todolist;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoList from './TodoList';
 
 // ReactDOM将组件挂载到页面的某个节点下
 /* <App/> -->    JSX语法 */
 // 所有标签都为JSX语法 ，所以必须引入React
 // JSX语法中，如果我们要使用自己创建的组件，首字母必须大写
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
 
 

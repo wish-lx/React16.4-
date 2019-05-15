a08fc7d57470b8c3e1a439b60806df475b575591
18222478537@163.com <18222478537@163.com>
2019-05-15 21:01:23 +0800

    TransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React, {Fragment} from 'react'
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './styles.css'
 class App extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            show: true
+            list: []
         } 
         this.toggleClick = this.toggleClick.bind(this)
     }
     render(){
         return(
         <Fragment>
-            <CSSTransition
-            in={this.state.show}
-            timeout={1000}
-            classNames="fade"
-            onEntered={(el)=>{el.style.color = 'red'}}
-            appear={true}
-            >
-              <div>123</div>
-            </CSSTransition>
-              <button onClick={this.toggleClick}>toggle</button>
+            <TransitionGroup>
+            {
+                this.state.list.map((item, index)=> {
+                    return(
+                        <CSSTransition
+                        timeout={1000}
+                        classNames="fade"
+                        onEntered={(el)=>{el.style.color = 'red'}}
+                        appear={true}
+                        key={index}
+                        > 
+                         <div >{item}</div>
+                     </CSSTransition>
+                    )
+                })
+            }
+            </TransitionGroup>
+            <button onClick={this.toggleClick}>toggle</button>
         </Fragment>
         )
     }
     toggleClick(){
-        this.setState({
-           show: this.state.show ? false : true
+        this.setState((preState)=>{
+            return{
+                list: [...preState.list, 'item']
+            }
         })
     }
 }

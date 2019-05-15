511fc412d2a2da71686183bd9eff611ca07131de
18222478537@163.com <18222478537@163.com>
2019-05-15 18:53:08 +0800

    App组件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+// import TodoList from './TodoList';
+import App from './App'
 
 // ReactDOM将组件挂载到页面的某个节点下
 /* <App/> -->    JSX语法 */
 // 所有标签都为JSX语法 ，所以必须引入React
 // JSX语法中，如果我们要使用自己创建的组件，首字母必须大写
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 

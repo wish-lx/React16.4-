e19f1ce68cf1d7aecf6ea190be8781a778e32808
18222478537@163.com <18222478537@163.com>
2019-05-15 21:11:17 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component{
                         > 
                          <div >{item}</div>
                      </CSSTransition>
-                    )
+                    ) 
                 })
             }
             </TransitionGroup>

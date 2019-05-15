8fcc591ecc8cb277dba7d9736e64ecd37d82ba8d
18222478537@163.com <18222478537@163.com>
2019-05-15 22:29:21 +0800

    Antd组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import 'antd/dist/antd.css';
-import { Input } from 'antd'
+import { Input, Button } from 'antd'
 class todoList extends React.Component{
     render(){
         return ( 
-            <div>
-               <Input placeholder="Basic usage" style={{width: 300}}/>
-               
+            <div style={{marginTop:'20px', marginLeft: '20px'}}>
+               <Input placeholder="Basic usage" style={{width: 300,marginRight: 20}}/>
+               <Button type="primary">提交</Button>
             </div>
         
         )

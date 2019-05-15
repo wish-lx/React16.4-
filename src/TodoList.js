f1f5a0463aee4e01c2ee549e471d8918358fb89c
18222478537@163.com <18222478537@163.com>
2019-05-15 21:42:08 +0800

    Antd

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,83 +1,15 @@
-import React from 'react';
-import TodoItem from './todoItem.js'
-import axios from 'axios'
-import './style.css'
-
-class Todolist extends React.Component {
-
-	constructor(props) { 
-		super(props)
-		// 当组件的state或者props发生改变的时候，render函数就会重新执行
-		this.state = {
-			inputValue: '',
-			list: []
-		}
-		this.handleInputChange = this.handleInputChange.bind(this)
-		this.handleBtnChange = this.handleBtnChange.bind(this)
-	}
-	render() {
-		console.log('render')
-		return (
-			<div>
-				<div>
-					<label htmlFor="interArea">输入内容</label>
-					<input id="interArea" className='input' value={this.state.inputValue} onChange={this.handleInputChange} />
-					<button onClick={this.handleBtnChange}>提交</button></div>
-				<ul>
-					{
-						this.getTodoItem()
-					}
-				</ul>
-				{/* <Text content={this.state.inputValue}></Text> */}
-			</div>
-		)
-	}
-	componentDidMount(){
-	   axios.get('/api/todolist')
-	   .then((res)=>{
-		   console.log(res.data)
-		   this.setState(()=>({
-			   list: [...res.data]
-		   }))
-	   }).catch(()=>{
-		   alert('error')
-	   })
-	}
-	getTodoItem() {
-		return this.state.list.map((item, index) => {
-			return (
-				<div key={item}>
-					<TodoItem content={item}
-						index={index}
-						deleteItem={this.handleItemDelete.bind(this)} />
-					{/*  <li key={index}  onClick={this.handleItemDelete.bind(this, index)}
-						dangerouslySetInnerHTML={{__html: item}}></li> */}
-				</div>
-			)
-
-		})
-	}
-	handleItemDelete(index) {
-		// immutable语法
-		// state不允许我们做任何改变
-		this.setState((preState) => {
-			const list = [...preState.list]
-			list.splice(index, 1)
-			return { list }
-		})
-	}
-	handleBtnChange(e) {
-		this.setState((preState) => ({
-			list: [...preState.list, preState.inputValue],
-			inputValue: ''
-		}))
-	}
-	handleInputChange(e) {
-		const value = e.target.value
-		this.setState(() => ({
-			inputValue: value
-		}))
-	}
-
+import React from 'react'
+import 'antd/dist/antd.css';
+import { Input } from 'antd'
+class todoList extends React.Component{
+    render(){
+        return ( 
+            <div>
+               <Input placeholder="Basic usage" style={{width: 300}}/>
+               
+            </div>
+        
+        )
+    }
 }
-export default Todolist;
+export default todoList

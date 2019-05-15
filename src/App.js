122b03956e9501e1c26f36f8214d96beaac5f588
18222478537@163.com <18222478537@163.com>
2019-05-15 19:20:55 +0800

    简单css动画效果

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import './styles.css'
 class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            show: true
+        } 
+        this.toggleClick = this.toggleClick.bind(this)
+    }
     render(){
-        return <div>123</div>
+        return(
+        <Fragment>
+              <div className={this.state.show ? 'show' : 'hide'}>123</div>
+              <button onClick={this.toggleClick}>toggle</button>
+        </Fragment>
+        )
+    }
+    toggleClick(){
+        this.setState({
+           show: this.state.show ? false : true
+        })
     }
 }
 export default App

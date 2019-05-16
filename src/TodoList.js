b8f56919194b05d491c01ad9ff7cec89f055f8ef
18222478537@163.com <18222478537@163.com>
2019-05-16 16:44:07 +0800

    redux todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,14 +6,24 @@ import store from './store'
 class todoList extends React.Component{
      constructor(props){
          super(props);
+        //  获取store中的数据
          this.state = store.getState()
-         console.log(store.getState())
+        //  console.log(store.getState())
+         this.handleOnChange = this.handleOnChange.bind(this)
+         this.handler = this.handler.bind(this)
+         this.bClick = this.bClick.bind(this)
+        //  store中的数据发生变化便会调用subscribe方法
+         store.subscribe(this.handler)
      }
     render(){
         return ( 
             <div style={{marginTop:'20px', marginLeft: '20px'}}>
-               <Input value={this.state.inputValue} placeholder="Basic usage" style={{width: 300,marginRight: 20}}/>
-               <Button type="primary">提交</Button>
+               <Input value={this.state.inputValue} 
+               placeholder="Basic usage" 
+               style={{width: 300,marginRight: 20}}
+               onChange={this.handleOnChange}
+               />
+               <Button type="primary" onClick={this.bClick}>提交</Button>
                <List
                 bordered
                 dataSource={this.state.list}
@@ -24,6 +34,24 @@ class todoList extends React.Component{
             </div>
         
         )
+    }
+    handleOnChange(e){
+        // 创建action
+        const action = {
+            type: 'type-input-value',
+            value: e.target.value
+        }
+        // 传送给state
+        store.dispatch(action)
+    }
+    handler() {
+        this.setState(store.getState())
+    }
+    bClick() {
+        const action = {
+            type: 'add-item'
+        }
+        store.dispatch(action)
     }
 }
 export default todoList
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,5 +3,20 @@ const defaultState={
     list: [1,2,3]
 }
 export default (state = defaultState, action)=> {
+    // 接收state，action
+    // console.log(state, action)
+    if(action.type === 'type-input-value') { 
+        // 对原始数据进行深拷贝，并且将传入的 action 的值赋值，并return给组件
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return newState
+    }
+    if(action.type === 'add-item') { 
+        // 对原始数据进行深拷贝，并且将传入的 action 的值赋值，并return给组件
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
     return state
 }

951548facef304ce7fd5873931152f6d88d5d233
18222478537@163.com <18222478537@163.com>
2019-05-16 16:01:42 +0800

    redux  devtools简单使用

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,16 +6,17 @@ import store from './store'
 class todoList extends React.Component{
      constructor(props){
          super(props);
+         this.state = store.getState()
          console.log(store.getState())
      }
     render(){
         return ( 
             <div style={{marginTop:'20px', marginLeft: '20px'}}>
-               <Input placeholder="Basic usage" style={{width: 300,marginRight: 20}}/>
+               <Input value={this.state.inputValue} placeholder="Basic usage" style={{width: 300,marginRight: 20}}/>
                <Button type="primary">提交</Button>
                <List
                 bordered
-                dataSource={[]}
+                dataSource={this.state.list}
                 renderItem={item => (
                     <List.Item> {item}</List.Item>
                 )}

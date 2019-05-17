7d34c360fe0f559bb5a6fae2ede0018d4d68247e
18222478537@163.com <18222478537@163.com>
2019-05-17 09:47:41 +0800

    无状态组件

--- src/todoListUI.js
+++ src/todoListUI.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import { Input, Button, List} from 'antd'
-class TodoListUI extends React.Component{
-    render(){
-        return (
-            <div style={{marginTop:'20px', marginLeft: '20px'}}>
-               <Input value={this.props.inputValue} 
-               placeholder="Basic usage" 
-               style={{width: 300,marginRight: 20}}
-               onChange={this.props.handleOnChange}
-               />
-               <Button type="primary" onClick={this.props.bClick}>提交</Button>
-               <List
-                bordered
-                dataSource={this.props.list}
-                renderItem={(item, index) => ( 
-                    <List.Item onClick={()=>{this.props.delete(index)}}> {item}</List.Item>
-                )}
-                style={{width: 300}}/>
-            </div>
-        )
-    }
+// 无状态组件（如果一个组件里面只有一个render函数，则可以用无状态组件进行代替）
+const TodoListUI = (props) => {
+    return (
+        <div style={{marginTop:'20px', marginLeft: '20px'}}>
+        <Input value={this.props.inputValue} 
+        placeholder="Basic usage" 
+        style={{width: 300,marginRight: 20}}
+        onChange={this.props.handleOnChange}
+        />
+        <Button type="primary" onClick={props.bClick}>提交</Button>
+        <List
+         bordered
+         dataSource={props.list}
+         renderItem={(item, index) => ( 
+             <List.Item onClick={()=>{props.delete(index)}}> {item}</List.Item>
+         )}
+         style={{width: 300}}/>
+     </div>
+    )
 }
+// class TodoListUI extends React.Component{
+//     render(){
+//         return (
+            
+//         )
+//     }
+// }
 export default TodoListUI
